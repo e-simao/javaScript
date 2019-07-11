24ff56656e5c51c932d607b6096e5d4a3e782f5c
Valentina <vaahxx@gmail.com>
2019-07-11 10:25:22 -0300

    inserindo novos arquivos

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log ("Média Final")
+
+let nomeDoUsuario = prompt ("Qual o seu nome");
+let ComoForamAsProvas = prompt ("Você foi bem na Prova")
+let nota = prompt ("Qual foi sua nota Final");
+let media = 7;
+
+if (Number(nota) >= media) {
+    alert ('Parabéms você está aprovado!')
+}
+
+else {
+    alert ('Que pena, você foi Reprovado!')
+}
